ad29520637a9cfc908472c1cce6184140d5ad370
Cole Gawin <colegawin@gmail.com>
2025-06-12 17:05:03 -0700

    square alumni spotlight cards

--- src/app/alumni-spotlight/smooth-infinite-carousel.tsx
+++ src/app/alumni-spotlight/smooth-infinite-carousel.tsx
@@ -81,7 +81,7 @@ export default function SmoothInfiniteCarousel({
       <div ref={innerRef} className="flex">
         {items.map((item, index) => (
           <div key={index} className="flex-shrink-0 px-2">
-            <div className="relative flex h-72 w-80 flex-col items-start justify-between border border-neutral-600 bg-neutral-900 p-6 text-left text-white">
+            <div className="relative flex size-80 flex-col items-start justify-between border border-neutral-600 bg-neutral-900 p-6 text-left text-white">
               <div className="text-sm text-neutral-400">{item.founders}</div>
 
               <div>
