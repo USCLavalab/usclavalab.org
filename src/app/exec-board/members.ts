e2d1857589d5f472c196f7fbeaad2afe193d842b
Ethan Palosh <109639295+epalosh@users.noreply.github.com>
2025-08-29 17:33:01 -0700

    Ethan: Updated E-board for F'25

--- src/app/exec-board/members.ts
+++ src/app/exec-board/members.ts
@@ -1,93 +1,110 @@
-import advikImg from "./images/advik.png";
-import alexImg from "./images/alex.png";
-import codyImg from "./images/cody.png";
+import allisonImg from "./images/allison.jpg";
+import amritaImg from "./images/amrita.jpg";
+import arinImg from "./images/arin.jpg";
+import atticusImg from "./images/atticus.jpg";
+import bodieImg from "./images/bodie.jpg";
+import bryanImg from "./images/bryan.jpg";
 import coleImg from "./images/cole.png";
-import irisImg from "./images/iris.png";
-import jerryImg from "./images/jerry.png";
-import joshetaImg from "./images/josheta.png";
-import mantejImg from "./images/mantej.png";
-import mohamedImg from "./images/mohamed.png";
-import riyaImg from "./images/riya.png";
-import uyenImg from "./images/uyen.png";
+import ethanImg from "./images/ethan.jpg";
+import moImg from "./images/mo.jpg";
+import naysaImg from "./images/naysa.jpg";
+import sarahImg from "./images/sarah.jpg";
+import sonyaImg from "./images/sonya.jpg";
+import xinyueImg from "./images/xinyue.jpg";
 
 const members = [
   {
-    name: "Josheta Srinivasan",
-    role: "President",
-    cohort: "Cohort S'23",
-    image: joshetaImg,
-    url: "https://www.linkedin.com/in/joshetasrinivasan/",
-  },
-  {
-    name: "Cody Chen",
-    role: "Vice President",
-    cohort: "Cohort S'24",
-    image: codyImg,
-    url: "https://www.linkedin.com/in/codyachen/",
-  },
-  {
-    name: "Alex Forgosh",
-    role: "Director of Operations",
-    cohort: "Cohort F'23",
-    image: alexImg,
-    url: "https://www.linkedin.com/in/forgosh/",
-  },
-  {
-    name: "Advik Unni",
-    role: "Director of PM",
-    cohort: "Cohort S'24",
-    image: advikImg,
-    url: "https://www.linkedin.com/in/advik-unni/",
-  },
-  {
-    name: "Iris Leung",
-    role: "Director of Design",
-    cohort: "Cohort F'21",
-    image: irisImg,
-    url: "https://www.linkedin.com/in/iris-c-leung/",
-  },
-  {
     name: "Cole Gawin",
-    role: "Director of Development",
+    role: "President",
     cohort: "Cohort S'24",
     image: coleImg,
     url: "https://www.linkedin.com/in/colegawin/",
   },
   {
+    name: "Xinyue Ma",
+    role: "Vice President",
+    cohort: "Cohort S'23",
+    image: xinyueImg,
+    url: "https://www.linkedin.com/in/xxinyuema/",
+  },
+  {
+    name: "Bryan Phillips",
+    role: "Director of Recruitment",
+    cohort: "Cohort F'23",
+    image: bryanImg,
+    url: "https://www.linkedin.com/in/bryan-phillips12/",
+  },
+  {
     name: "Mohamed Ahmed",
-    role: "Director of Industry & Sponsorships",
+    role: "Director of Curriculum",
     cohort: "Cohort F'24",
-    image: mohamedImg,
+    image: moImg,
     url: "https://www.linkedin.com/in/0xmo/",
   },
   {
-    name: "Uyen Hoang",
-    role: "Director of Marketing",
-    cohort: "Cohort S'24",
-    image: uyenImg,
-    url: "https://www.linkedin.com/in/nhatuyenhoang/",
+    name: "Atticus Kim",
+    role: "Director of PM",
+    cohort: "Cohort F'24",
+    image: atticusImg,
+    url: "https://www.linkedin.com/in/atticuskim/",
   },
   {
-    name: "Mantej Lamba",
-    role: "Co-Director of Internal Community",
-    cohort: "Cohort F'23",
-    image: mantejImg,
-    url: "https://www.linkedin.com/in/mantejlamba/",
+    name: "Allison Huang",
+    role: "Director of Design",
+    cohort: "Cohort S'23",
+    image: allisonImg,
+    url: "https://www.linkedin.com/in/allisoncyhuang/",
   },
   {
-    name: "Jerry Zhang",
+    name: "Ethan Palosh",
+    role: "Director of Development",
+    cohort: "Cohort S'25",
+    image: ethanImg,
+    url: "https://www.linkedin.com/in/ethanpalosh/",
+  },
+  {
+    name: "Amrita Arun",
+    role: "Director of External Community",
+    cohort: "Cohort F'24",
+    image: amritaImg,
+    url: "https://www.linkedin.com/in/amrita-arun/",
+  },
+  {
+    name: "Naysa Bhargava",
     role: "Co-Director of Internal Community",
     cohort: "Cohort F'24",
-    image: jerryImg,
-    url: "https://www.linkedin.com/in/jerry-n-zhang/",
+    image: naysaImg,
+    url: "https://www.linkedin.com/in/naysa-bhargava/",
   },
   {
-    name: "Riya Shenoy",
-    role: "Director of External Community",
-    cohort: "Cohort S'23",
-    image: riyaImg,
-    url: "https://www.linkedin.com/in/riyashenoy/",
+    name: "Arin Jain",
+    role: "Co-Director of Internal Community",
+    cohort: "Cohort S'25",
+    image: arinImg,
+    url: "https://www.linkedin.com/in/arinjain1/",
   },
+  {
+    name: "Bodie Currier",
+    role: "Director of Publicity",
+    cohort: "Cohort F'24",
+    image: bodieImg,
+    url: "https://www.linkedin.com/in/bodiecurrier/",
+  },
+  {
+    name: "Sarah Kwang",
+    role: "Director of Marketing",
+    cohort: "Cohort F'24",
+    image: sarahImg,
+    url: "https://www.linkedin.com/in/sarah-kwang/",
+  },
+  {
+    name: "Sonya Alexis",
+    role: "Director of Industry Relations",
+    cohort: "Cohort S'25",
+    image: sonyaImg,
+    url: "https://www.linkedin.com/in/sonya-alexis/",
+  },
+
 ];
 
 export default members;
